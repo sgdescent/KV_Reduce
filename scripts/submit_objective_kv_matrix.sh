5d7ebb253fd10710b2680dad52f84bc145efab10
higgs <higgsboson1209@gmail.com>
2026-08-09 22:41:04 -0700

    Allow untracked objective matrix smoke runs

--- scripts/submit_objective_kv_matrix.sh
+++ scripts/submit_objective_kv_matrix.sh
@@ -11,6 +11,7 @@ SEEDS="${SEEDS:-0,1,2}"
 NUM_EVAL="${NUM_EVAL:-32}"
 NUM_LAYERS="${NUM_LAYERS:-28}"
 MAX_CONCURRENT="${MAX_CONCURRENT:-2}"
+ENABLE_WANDB="${ENABLE_WANDB:-1}"
 EXCLUDE_NODES="${EXCLUDE_NODES:-catalyst-0-9,catalyst-0-15}"
 SOURCE_DEPENDENCY="${SOURCE_DEPENDENCY:-}"
 
@@ -29,7 +30,7 @@ prep=$(sbatch --parsable --exclude="$EXCLUDE_NODES" "${dependency_args[@]}" \
   scripts/prepare_objective_kv_matrix.slurm)
 array=$(sbatch --parsable --exclude="$EXCLUDE_NODES" --dependency="afterok:$prep" \
   --array="0-$((num_tasks - 1))%$MAX_CONCURRENT" \
-  --export=ALL,MANIFEST="$MATRIX_ROOT/manifest.tsv",WANDB_PROJECT=kv-reduce,WANDB_GROUP=objective-matrix \
+  --export=ALL,MANIFEST="$MATRIX_ROOT/manifest.tsv",ENABLE_WANDB="$ENABLE_WANDB",WANDB_PROJECT=kv-reduce,WANDB_GROUP=objective-matrix \
   scripts/eval_objective_kv_matrix.slurm)
 aggregate=$(sbatch --parsable --exclude="$EXCLUDE_NODES" --dependency="afterok:$array" \
   --export=ALL,MATRIX_DIR="$MATRIX_ROOT",OUT_DIR="$MATRIX_ROOT/aggregate" \
